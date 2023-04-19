4b166b563e93e7e8f999813be1e8f2e1765b1691
Aster110 <114595163+Aster110@users.noreply.github.com>
2023-04-19 13:46:37 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+import { Counter } from "./counter.js";
+import { Settings } from "./settings.js";
+
+const counter = new Counter();
+const settings = new Settings();
+
+counter.init();
+settings.init();
